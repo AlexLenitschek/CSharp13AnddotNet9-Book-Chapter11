0eec77a0ce2f4920076e10d1750d0e4272156212
AlexLenitschek <alexander.lenitschek@gmx.de>
2025-03-04 16:22:58 +0100

    Adding Linq Query Exercise of chapter11

---
+++ Exercise_LinqQueries/EntityModels/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations; // To use [Required] and [StringLength]
+
+namespace Northwind.EntityModels;
+
+public class Customer
+{
+    [Required]
+    public string CustomerId { get; set; } = null!;
+
+
+    [StringLength(40)]
+    public string CompanyName { get; set; } = null!;
+
+
+    [StringLength(30)]
+    public string? City { get; set; }
+}
---
+++ Exercise_LinqQueries/Program_Functions.cs
@@ -0,0 +1,40 @@
+﻿using Northwind.EntityModels; // To use NorthwindDb, Category and Product.
+using Microsoft.EntityFrameworkCore; // To use DbSet<T>.
+partial class Program
+{
+    private static void customersInCity()
+    {
+        SectionTitle("Customers in City");
+
+        using NorthwindDb db = new();
+
+
+        IQueryable<string?> distinctCities = db.Customers.
+            Select(c => c.City).Distinct();
+
+        WriteLine("A list of cities that at least one customer resides in:");
+        WriteLine($"{string.Join(", ", distinctCities)}");
+        WriteLine();
+
+
+        Write("Enter the name of a city: ");
+        string inputCity = ReadLine()!;
+
+        IQueryable<Customer> customersInSpecificCity = db.Customers
+            .Where(customer => customer.City == inputCity);
+
+        var filteredCustomers = customersInSpecificCity
+            .Select(customer => new
+            {
+                customer.CompanyName,
+                customer.City
+            });
+
+        WriteLine($"There are {filteredCustomers.Count()} customers in {inputCity}:");
+
+        foreach (var customer in filteredCustomers)
+        {
+            WriteLine($"  {customer.CompanyName}");
+        }
+    }
+}
