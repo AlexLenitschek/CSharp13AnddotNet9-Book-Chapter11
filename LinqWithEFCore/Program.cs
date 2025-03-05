f448676a842b150ad698daea26bd08c597622d22
alex lenitschek <Alexander.lenitschek@gmx.de>
2025-03-05 15:37:08 +0100

    adding aggregation stuff to chapter11

--- LinqWithEFCore/Program.cs
+++ LinqWithEFCore/Program.cs
@@ -2,4 +2,31 @@
 //FilterAndSort();
 //JoinCategoriesAndProducts();
 //GroupJoinCategoriesAndProducts();
-ProductsLookup();
+//ProductsLookup();
+//AggregateProducts();
+PagingProducts();
+
+
+#region Information on LINQ extensions and lambda expressions vs. query comprehension syntax
+//Consider the following array of string values:
+//```cs
+//string[] names = new[] { "Michael", "Pam", "Jim", "Dwight",
+//  "Angela", "Kevin", "Toby", "Creed" };
+//```
+
+//To filter and sort the names, you could use extension methods and lambda expressions, as shown in the following code:
+//```cs
+//var query = names
+//  .Where(name => name.Length > 4)
+//  .OrderBy(name => name.Length)
+//  .ThenBy(name => name);
+//```
+
+//Or you could achieve the same results by using query comprehension syntax, as shown in the following code:
+//```cs
+//var query = from name in names
+//  where name.Length > 4
+//  orderby name.Length, name
+//  select name;
+//```
+#endregion
